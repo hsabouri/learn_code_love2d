a829f26b38a3a7d4c8ace4dc459f275c451b7f3b
hsabouri <hsabouri@student.42.fr>
2017-11-28 16:13:39 +0100

    sdfjkjh

--- 5_project/main.lua
+++ 5_project/main.lua
@@ -3,5 +3,12 @@
 -- Create a simplified snake game
 -- 	- The player is a "0" moving in the window with keyboard arrows
 --	- Some food ("*") is randomly placed in the window
---	- The player has to eat the food by moving on it
---	- 
+--	- The player has to eat the food by moving over it
+--	- When player eats food, new food is placed randomly into the window
+--	- When player eats food, 0 become 1 then 2 etc...
+--	- Player and Food should not be able to go out of the window
+--	- When pressing "escape", game quits
+
+
+
+-- hint: https://love2d.org/wiki/love.math.random
